7df0bcb8a8cd6ff4d17bdbe27129be4a84596aa8
tomcraig99 <tomcraig99@gmail.com>
2022-03-28 11:37:11 -0500

    removed thing

--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -37,9 +37,6 @@ for i in original_list:
 # 1 Using a list comprehension, create a new list called "newlist" out of the list "numbers", 
 # which contains only the positive numbers from the list, as integers.
 
-from sqlite3 import TimestampFromTicks
-
-
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
 newlist = [x for x in numbers if x>=0]
